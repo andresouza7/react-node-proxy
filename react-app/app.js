99f0c795b64c627e1142882f2fc3e0f6773918df
Andre <souza.andre1993@gmail.com>
2020-07-24 12:56:45 -0300

    server ready

---
+++ react-app/app.js
@@ -0,0 +1,2 @@
+// In your react app:
+// just forward the request to http://localhost: + whatever port your server is using
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const axios = require('axios').default;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// limit requests
+const limiter = rateLimit({
+	windowMs: 60 * 1000, // 1 minute
+	max: 20, // limit each IP to 100 requests per windowMs
+	message: 'too many requests, try again in a minute',
+});
+
+//  apply to all requests
+app.use(limiter);
+
+// configure cache
+let cachedData;
+let cacheTime;
+
+// redirect calls
+app.get('/', (req, res) => {
+	console.log('ping');
+	const BASE_URL = 'https://jobs.github.com/positions.json';
+	const params = req.query;
+	console.log(params);
+	// check for cached data
+	// if (cachedData && cacheTime > Date.now() - 1 * 60 * 1000){
+    //     console.log('sending cached content');
+    //     return res.send(cachedData);
+    // }
+	axios
+		.get(BASE_URL, {
+			params: params,
+		})
+		.then(function (response) {
+			// handle success
+			console.log('sending brand new content');
+			cachedData = response.data;
+			cacheTime = Date.now();
+			res.send(cachedData);
+		})
+		.catch(function (error) {
+			// handle error
+			console.log(error);
+		});
+});
+
+app.listen(4000, () => {
+	console.log('server started on port 4000');
+});
